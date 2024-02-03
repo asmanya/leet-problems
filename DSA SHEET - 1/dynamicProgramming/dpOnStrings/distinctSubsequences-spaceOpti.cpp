a2da067a29d099fc2240150dfb71c721b1869386
asmanya <ishuisu147@gmail.com>
2024-02-03 22:25:36 +0530

    dp

---
+++ DSA SHEET - 1/dynamicProgramming/dpOnStrings/distinctSubsequences-spaceOpti.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n = s.length(), m = t.length();
+        vector<double> prev(m + 1, 0);
+
+        prev[0] = 1;
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = m; j >= 1; j--) {
+                if(s[i - 1] == t[j -1]) {
+                    prev[j] += prev[j-1];
+                }
+            }
+        }
+
+        return (int)prev[m];
+    }
+};
