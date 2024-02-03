10b663c91baf0e95332c9cdedb2300079b393fb8
asmanya <ishuisu147@gmail.com>
2024-02-03 17:06:28 +0530

    dp

---
+++ DSA SHEET - 1/dynamicProgramming/dpOnStrings/delOpTwoStrings-tabular.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.length(), n = word2.length();
+        vector<int> prev(n + 1, 0), cur(n +1, 0);
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
+                if(word1[i-1] == word2[j-1]) {
+                    cur[j] = 1 + prev[j-1];
+                } else {
+                    cur[j] =  max(prev[j], cur[j-1]);
+                }
+            }
+            prev = cur;
+        }
+
+        return (m + n - (2 * prev[n]));
+    }
+};
--- DSA SHEET - 1/dynamicProgramming/dpOnStrings/longestCommonSubsequence-spaceOpti.cpp
+++ DSA SHEET - 1/dynamicProgramming/dpOnStrings/longestCommonSubsequence-spaceOpti.cpp
@@ -5,18 +5,20 @@ class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
         int m = text1.length(), n = text2.length();
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
-        
-        for(int i = 1; i <- n; i++) {
-            for(int j = 1; j <= m; j++) {     
+        vector<int> prev(n + 1, 0), cur(n +1, 0);
+
+        for(int i = 1; i <= m; i++) {
+            for(int j = 1; j <= n; j++) {
                 if(text1[i-1] == text2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    cur[j] = 1 + prev[j-1];
                 } else {
-                    dp[i][j] =  max(dp[i -1][j], dp[i][j-1]);
+                    cur[j] =  max(prev[j], cur[j-1]);
                 }
             }
+            prev = cur;
         }
 
-        return dp[n][m];
+        return prev[n];
     }
 };
+
