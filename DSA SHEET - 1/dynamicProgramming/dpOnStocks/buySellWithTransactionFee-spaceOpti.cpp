3761aa321ce951624f451b8681b7ec76172b4e9f
asmanya <ishuisu147@gmail.com>
2024-02-09 23:05:43 +0530

    dp

--- DSA SHEET - 1/dynamicProgramming/dpOnStocks/buySellWithTransactionFee-spaceOpti.cpp
+++ DSA SHEET - 1/dynamicProgramming/dpOnStocks/buySellWithTransactionFee-spaceOpti.cpp
@@ -3,24 +3,23 @@ using namespace std;
 
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
+    int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        vector<int> cur(2, 0), ahead(2, 0), ahead2(2, 0);
+        vector<int> cur(2, 0), ahead(2, 0);
 
-        int profit;
+        int profit = 0;
         for(int ind = n-1; ind >= 0; ind--) {
-            for(int buy = 0; buy < 2; buy++) {
+            for(int buy = 0; buy < 2; buy++) {       
                 if(buy) {
                     profit = max(-prices[ind] + ahead[0], ahead[1]);
                 } else {
-                    profit = max(prices[ind] + ahead2[1], ahead[0]);
+                    profit = max(prices[ind] - fee + ahead[1], ahead[0]);
                 }
                 cur[buy] = profit;
             }
-            ahead2 = ahead;
             ahead = cur;
         }
-
+        
         return cur[1];
     }
 };
